9650831bcb70e5b337aee08ac55615a54e5cdd7e
Saar Raz <saarraz1@gmail.com>
2016-03-25 18:34:05 +0300

    [-] Working version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # range-arithmetic
-A javascript library for adding and subtracting of numeric ranges, e.g. [[0, 2], [3, 8]] - [[1, 7]] -> [[0, 1], [7, 8]]
+
+[![NPM Downloads](https://img.shields.io/npm/dm/range-arithmetic.svg)](https://www.npmjs.org/package/range-arithmetic)
+[![NPM Version](https://img.shields.io/npm/v/range-arithmetic.svg)](https://www.npmjs.org/package/range-arithmetic)
+
+A javascript library for addition and subtraction of numeric ranges and sets of ranges.
+
+## Getting Started
+Install the module with: `npm install --save range-arithmetic`
+
+## Examples
+The library exports the Range class, which represents a not-necessarily contiguous range of numbers.
+
+Import the library:
+```
+var Range = require('range-arithmetic');
+```
+ES6+:
+```
+import Range from 'range-arithmetic';
+```
+
+Initialize a Range object:
+```
+const r1 = new Range(3, 4); // OK - length 1 range including only 3 (Ranges are '[ )').
+const r2 = new Range([2, 3], [4, 6]); // OK - includes 2, 4, 5
+const r3 = new Range([[-3, 5], [8, 9]]); // OK - includes -3, -2, -1, 0, 1, 2, 3, 4, 8
+const r4 = new Range(); // OK - empty range
+const r5 = new Range(3.4, 4.21); // OK - floats are supported.
+const r6 = new Range(Range.MIN, 10); // OK - includes all real numbers up to 10.
+```
+
+[tiny-range](https://www.npmjs.com/package/tiny-range) syntax is also supported for initialization, though initialization with it is inclusive on both ends.
+```
+const r7 = new Range('0, 1, 7~8, 9-10, 100~105'); // OK - includes 0, 1, 7, 8, 9, 10, 100, 101, 102, 103, 104, 105
+```
+
+
+Wrong ways to initialize:
+```
+new Range(9); // ERROR - supporting this would be inconsistent when using floats.
+new Range(9.2); // ERROR - does this include 9.2 - 10.2? not supported.
+new Range('a', 'b') // ERROR - numeric ranges only.
+new Range(3, 2) // ERROR - start must come before end
+new Range(3, 2, 9) // ERROR - what does this mean?
+new Range([3, 5], [0, 2]) // ERROR - sub-ranges must be sorted.
+new Range([3, 5], [2, 6]) // ERROR - sub-ranges must be non-overlapping.
+```
+
+Queries
+```
+console.log(r1.isContiguous()) // Outputs true
+console.log(r3.isContiguous()) // Outputs false
+console.log(r1.length()) // Outputs 1
+console.log(r3.length()) // Outputs 9
+console.log(r3.enclosingContiguousRange()) // Outputs [-3 9)
+console.log(r1.empty()) // Outputs false
+console.log(r4.empty()) // Outputs true
+console.log(r2.includes(r3)) // Outputs true
+console.log(r1.equals(new Range(3, 4))) // Outputs true
+```
+
+Do arithmetic!
+```
+console.log(r1.add(r2)) // Outputs [[2 6)]
+console.log(r2.add(r3)) // Outputs [[-3 5), 8]
+console.log(r1.add(r4)) // Outputs [3]
+console.log(r3.subtract(r1)) // Outputs [[-3 3), 4, 8]
+```
---
+++ src/Range.js
@@ -0,0 +1,213 @@
+import TinyRange from 'tiny-range';
+import isNumber from 'is-number';
+import assert from 'assert';
+
+class ContiguousRange {
+    constructor(x, y) {
+        if (y == undefined) {
+            if (typeof x === 'string') {
+                // Range('0-2')
+                const range = TinyRange.parse(x);
+                assert(range.length == 1);
+                this.start = range.start;
+                this.end = range.end;
+            } else if (Array.isArray(x)) {
+                // Range([0, 2])
+                assert(
+                    x.length == 2 
+                    && isNumber(x[0]) 
+                    && isNumber(x[1])
+                    && x[0] <= x[1],
+                    'Array initializer must contain 2 numbers, the first > the second.'
+                );
+                this.start = x[0];
+                this.end = x[1];
+            } else if (x instanceof ContiguousRange) {
+                this.start = x.start;
+                this.end = x.end;
+            } else {
+                assert(false, 'Invalid ContiguousRange constructor arguments: ' + Array.prototype.slice.call(arguments));
+            }
+        } else {
+            // Range(0, 2)
+            assert(isNumber(x) && isNumber(y) && x <= y);
+            this.start = x;
+            this.end = y;
+        }
+        
+    }
+    
+    toString() {
+        return this.length() == 1 ? this.start : '[' + this.start + ' ' + this.end + ')';
+    }
+    
+    length() {
+        return this.end - this.start;
+    }
+    
+    empty() {
+        return this.length() == 0;
+    }
+    
+    subtract(other) {
+        let distance = undefined;
+        if (this.start < other.start) {
+            if (this.end > other.start) {
+                if (this.end > other.end) {
+                    // [return[0].start return[0].end]                     [return[1].start return[1].end]
+                    // [this.start                   [other.start other.end]                     this.end]
+                    return new Range([this.start, other.start], [other.end, this.end]);
+                } else {
+                    // [return.start return.end]
+                    // [this.start             [other.start this.end] other.end]
+                    return new Range(this.start, other.start);
+                }
+            } else {
+                // [return.start return.end]                       
+                // [this.start     this.end] [other.start other.end]
+                return new Range(this);
+            }
+        } else {
+            if (other.end > this.start) {
+                if (other.end > this.end) {
+                    // return is empty.
+                    // [other.start [this.start this.end] other.end]
+                    return new Range();
+                } else {
+                    //                                    [return.start return.end]
+                    // [other.start [this.start  other.end]               this.end]
+                    return new Range(other.end, this.end);
+                }
+            } else {
+                //                         [return.start return.end]
+                // [other.start other.end] [this.start     this.end]
+                return new Range(this);
+            }
+        }
+    }
+}
+
+class Range {
+    
+    static MAX = Number.POSITIVE_INFINITY;
+    static MIN = Number.NEGATIVE_INFINITY;
+    
+    constructor() {
+        const args = Array.prototype.slice.call(arguments);
+        if (args.length == 0) {
+            // Range() -> empty range
+            this.contiguous_ranges = [];
+        } else if (args.length == 1) {
+            let a = args[0];
+            if (typeof a === 'string') {
+                    // Range('0-2, 4-7')
+                this.contiguous_ranges = TinyRange.parse(a);
+            } else if (Array.isArray(a)) {
+                if (Array.isArray(a[0])) {
+                    // Range([[0, 2], [4, 7]])
+                    const raw_ranges = a.map(contiguous_range_init => new ContiguousRange(contiguous_range_init));
+                    this.contiguous_ranges = [];
+                    raw_ranges.forEach(contiguous_range => {
+                        this.contiguous_ranges = this.add(new Range(contiguous_range)).contiguous_ranges;
+                    });
+                    console.log(this.contiguous_ranges);
+                } else if (isNumber(a[0])) {
+                    // Range([0, 2])
+                    this.contiguous_ranges = [new ContiguousRange(a)];
+                } else if (a[0] instanceof ContiguousRange) {
+                    // Range([[new ContiguousRange(0, 1), new ContiguousRange(2, 4)]])
+                    this.contiguous_ranges = a.map(a => a);
+                } else {
+                    assert(false, 'Invalid Range constructor args: ' + args);
+                }
+            } else if (a instanceof Range) {
+                // Range(other_range)
+                this.contiguous_ranges = a.contiguous_ranges;
+            } else if (a instanceof ContiguousRange) {
+                this.contiguous_ranges = [a];
+            } else {
+                assert(false, 'Invalid Range constructor args: ' + args);
+            }
+        } else if (args.length == 2 && isNumber(args[0])) {
+            // Range(0, 2);
+            this.contiguous_ranges = [new ContiguousRange(args)];
+        } else {
+            // Range([0, 2], [4, 7]);
+            this.contiguous_ranges = args.map(contiguous_range_init => new ContiguousRange(contiguous_range_init));
+        }
+    }
+    
+    toString() {
+        return '[' + this.contiguous_ranges.map(r => r.toString()).join(', ') + ']';
+    }
+    
+    empty() {
+        return this.length() == 0;
+    }
+    
+    length() {
+        return this.contiguous_ranges.reduce((total, contiguous_range) => total + contiguous_range.length(), 0);
+    }
+    
+    contiguousParts() {
+        return this.contiguous_ranges;
+    }
+    
+    isContiguous() {
+        return this.contiguous_ranges.length <= 1;
+    }
+    
+    enclosingContiguousRange() {
+        return this.isContiguous() ? this : new ContiguousRange(this.contiguous_ranges[0].start, this.contiguous_ranges[this.contiguous_ranges.length - 1].end);
+    }
+    
+    equals(other) {
+        return this.includes(other) && other.includes(this);
+    }
+    
+    includes(other) {
+        return this.subtract(other).empty()
+    }
+    
+    overlaps(other) {
+        return !this.subtract(other).equals(this);
+    }
+    
+    subtract(other) {
+        let difference_ranges = [];
+        this.contiguous_ranges.forEach(contiguous_range => {
+            let remaining = [contiguous_range];
+            other.contiguous_ranges.forEach(other_contiguous_range => {
+                let new_remaining = [];
+                remaining.forEach(remaining_contiguous_range => {
+                    new_remaining = new_remaining.concat(remaining_contiguous_range.subtract(other_contiguous_range).contiguous_ranges)
+                });
+                remaining = new_remaining;
+            });
+            difference_ranges = difference_ranges.concat(remaining);
+        });
+        return new Range(difference_ranges);
+    }
+    
+    add(other) {
+        if (other.empty()) {
+            return this;
+        }
+        if (this.empty()) {
+            return other;
+        }
+        const enclosing = new Range(
+            Math.min(
+                this.contiguous_ranges[0].start, 
+                other.contiguous_ranges[0].start
+            ), 
+            Math.max(
+                this.contiguous_ranges[this.contiguous_ranges.length - 1].end, 
+                other.contiguous_ranges[other.contiguous_ranges.length - 1].end
+            )
+        );
+        return enclosing.subtract(enclosing.subtract(this).subtract(other));
+    }
+}
+
+module.exports = Range;
